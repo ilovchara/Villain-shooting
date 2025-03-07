8f75aa2e0b463d664deb55f66e514a97666cea63
chara_s <13093933+chara-s@user.noreply.gitee.com>
2025-03-07 13:35:33 +0800

    3-7

--- Assets/Scripts/Characters/Player/Player.cs
+++ Assets/Scripts/Characters/Player/Player.cs
@@ -1,105 +1,43 @@
-// 1. 玩家的移动效果 2. 玩家的射击效果
-using System;
-using UnityEditor;
-using UnityEngine;
-[RequireComponent(typeof(PlayerController))]
-[RequireComponent(typeof(GunController))]
-public class Player : LivingEntity
-{
-    // 玩家视角跟随鼠标
-    Camera viewCamera;
+﻿using UnityEngine;
+using System.Collections;
 
-    // 用于控制player的计算操作
-    PlayerController controller;
-    GunController gunController;
+[RequireComponent (typeof (PlayerController))]
+[RequireComponent (typeof (GunController))]
+public class Player : LivingEntity {
 
+	public float moveSpeed = 5;
 
-    [SerializeField] PlayerInput input;
-    [SerializeField] float moveSpeed = 5f;
-    new Rigidbody rigidbody;
+	Camera viewCamera;
+	PlayerController controller;
+	GunController gunController;
+	
+	protected override void Start () {
+		base.Start ();
+		controller = GetComponent<PlayerController> ();
+		gunController = GetComponent<GunController> ();
+		viewCamera = Camera.main;
+	}
 
+	void Update () {
+		// Movement input
+		Vector3 moveInput = new Vector3 (Input.GetAxisRaw ("Horizontal"), 0, Input.GetAxisRaw ("Vertical"));
+		Vector3 moveVelocity = moveInput.normalized * moveSpeed;
+		controller.Move (moveVelocity);
 
-    void Awake()
-    {
-        rigidbody = GetComponent<Rigidbody>();
+		// Look input
+		Ray ray = viewCamera.ScreenPointToRay (Input.mousePosition);
+		Plane groundPlane = new Plane (Vector3.up, Vector3.zero);
+		float rayDistance;
 
-    }
+		if (groundPlane.Raycast(ray,out rayDistance)) {
+			Vector3 point = ray.GetPoint(rayDistance);
+			//Debug.DrawLine(ray.origin,point,Color.red);
+			controller.LookAt(point);
+		}
 
-    protected override void Start()
-    {
-        base.Start();
-        controller = GetComponent<PlayerController>();
-        gunController = GetComponent<GunController>();
-        viewCamera = Camera.main;
-
-        input.EnableGamePlayerInput();
-
-    }
-
-    void Update()
-    {
-        Shooting();
-    }
-
-    #region 移动效果
-
-
-    [Obsolete]
-    void OnEnable()
-    {
-        input.onMove += Move;
-        input.onStopMove += StopMove;
-    }
-
-    [Obsolete]
-    void OnDisable()
-    {
-        input.onMove -= Move;
-        input.onStopMove -= StopMove;
-    }
-
-    [Obsolete]
-    private void StopMove()
-    {
-        rigidbody.velocity = Vector3.zero;
-    }
-
-    [Obsolete]
-    private Coroutine moveCoroutine;
-
-    [Obsolete]
-    private void Move(Vector3 moveInput)
-    {
-        // 持续按键输入时更新刚体的速度
-        Vector3 moveDirection = new Vector3(moveInput.x, 0, moveInput.y).normalized; // 保持方向一致
-        rigidbody.velocity = moveDirection * moveSpeed;
-    }
-
-    #endregion
-
-    #region 射击
-    // 玩家射击
-    void Shooting()
-    {
-        // 射线检测
-        Ray ray = viewCamera.ScreenPointToRay(Input.mousePosition);
-        Plane groundPlane = new Plane(Vector3.up, Vector3.zero);
-        float rayDistance;
-
-        if (groundPlane.Raycast(ray, out rayDistance))
-        {
-            Vector3 point = ray.GetPoint(rayDistance);
-            // 测试射线
-            // Debug.DrawLine(ray.origin, point, Color.red);
-            controller.LookAt(point);
-        }
-
-        // 射击
-        if (Input.GetMouseButton(0))
-        {
-            gunController.Shoot();
-        }
-
-    }
-    #endregion
+		// Weapon input
+		if (Input.GetMouseButton(0)) {
+			gunController.Shoot();
+		}
+	}
 }
--- Assets/Scripts/Characters/Player/PlayerController.cs
+++ Assets/Scripts/Characters/Player/PlayerController.cs
@@ -1,21 +1,32 @@
-using System;
-using System.Collections;
 using UnityEngine;
-// MVC设计模式 - Controller主要是控制player的行为
+using System.Collections;
+
 [RequireComponent(typeof(Rigidbody))]
 public class PlayerController : MonoBehaviour
 {
-    // 朝向问题
+
+    Vector3 velocity;
+    Rigidbody myRigidbody;
+
+    void Start()
+    {
+        myRigidbody = GetComponent<Rigidbody>();
+    }
+
+    public void Move(Vector3 _velocity)
+    {
+        velocity = _velocity;
+    }
+
     public void LookAt(Vector3 lookPoint)
     {
         Vector3 heightCorrectedPoint = new Vector3(lookPoint.x, transform.position.y, lookPoint.z);
         transform.LookAt(heightCorrectedPoint);
     }
 
+    void FixedUpdate()
+    {
+        myRigidbody.MovePosition(myRigidbody.position + velocity * Time.fixedDeltaTime);
 
-
-
-
-
-
+    }
 }
